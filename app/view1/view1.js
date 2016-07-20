81760eaeb95f6af1bc4914f50594d81768f59745
Amrit <amritpal1048@gmail.com>
2016-07-19 17:00:13 -0700

    working map

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [function($scope) {
+  //NgMap.getMap().then(function(map) {
+    //console.log(map.getCenter());
+    //console.log('markers', map.markers);
+    //console.log('shapes', map.shapes);
+  //});
+var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(41.923, 12.513),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+}
 
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }]);
+
+// var map;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: -34.397, lng: 150.644},
+//           zoom: 8
+//         });
+//       }
+//       //google.maps.event.addDomListener(window, "load", initialize);
